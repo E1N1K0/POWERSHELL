a14405dfee985fa399262cb881d43e71f1425511
Nicolas Daroch <69065853+E1N1K0@users.noreply.github.com>
2023-07-31 10:22:30 -0700

    Sube archivos

---
+++ Function.ps1
@@ -0,0 +1,8 @@
+﻿##Mi primer programa en powershell.
+##Crea la funcion add. Se llama al escribir add en comandos de power shell.
+
+function add
+  {
+  $add  =[int](2+2)
+  write-output "$add"
+  }
